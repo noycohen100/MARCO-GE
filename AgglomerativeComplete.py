1666baa2a2a55c561dde2a3f130090fc5cbefa14
noycohen100 <33723701+noycohen100@users.noreply.github.com>
2020-11-03 12:49:17 +0200

    Delete AgglomerativeComplete.py

--- AgglomerativeComplete.py
+++
@@ -1,7 +0,0 @@
-from sklearn.cluster import AgglomerativeClustering
-
-def get_AgglomerativeComplete(dataframe, K):
-    AgglomerativeComplete = AgglomerativeClustering(n_clusters=K, linkage="complete")
-    AgglomerativeComplete.fit(dataframe)
-
-    return AgglomerativeComplete.labels_
