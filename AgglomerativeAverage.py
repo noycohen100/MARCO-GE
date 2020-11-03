949759fd1e3c6ed9c5b5f75968537ea7b90a2452
noycohen100 <33723701+noycohen100@users.noreply.github.com>
2020-11-03 12:49:05 +0200

    Delete AgglomerativeAverage.py

--- AgglomerativeAverage.py
+++
@@ -1,7 +0,0 @@
-from sklearn.cluster import AgglomerativeClustering
-
-def get_AgglomerativeAverage(dataframe, K):
-    AgglomerativeAverage = AgglomerativeClustering(n_clusters=K, linkage="average")
-    AgglomerativeAverage.fit(dataframe)
-
-    return AgglomerativeAverage.labels_
