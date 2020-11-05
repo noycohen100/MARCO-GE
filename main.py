7d12b1c94c3ae79cb741eaf04c358eb61ff7d55a
noycohen100 <33723701+noycohen100@users.noreply.github.com>
2020-11-05 09:29:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Graph_representation_step import Graph_Representation
 from Clustering_algorithm_evaluation_step import Clustering_algorithm_evaluation
-from Meta-feature_generation_step import graphClassification
-from Meta-model_creation_step import model
+from Meta_feature_generation_step import graphClassification
+from Meta_model_creation_step import model
 import merge_results
 import argparse
 import os
@@ -22,13 +22,13 @@ def parse_args(parser):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run MARCO-GE")
     args = parse_args(parser)
-    os.mkdir(EdgeLists_folder)
-    os.mkdir(NodesEmbedding_folder)
-    Graph_Representation(args.Df)
+    Graph_Representation(args.Df, EdgeLists_folder, NodesEmbedding_folder)
+    print(args.Df)
     Clustering_algorithm_evaluation(args.Df, args.em)
     label_file_name = merge_results.find_best_algorithm(args.mm)
-    graphClassification(args.Df, EdgeLists_folder, NodesEmbedding_folder,args.ne, args.es, N_clustering_algs, args.mm,label_file_name)
-    with open('XGBoostHyperParameters/'+args.mm +".json", "w") as f:
-        parameters = json.load(f)
+    embedding_dir = graphClassification(args.Df, EdgeLists_folder+"/", NodesEmbedding_folder+"/",args.ne, args.es, N_clustering_algs, args.mm,label_file_name)
+
+    with open('XGBoostHyperParameters/'+args.mm +".json", "r") as fj:
+        parameters = json.load(fj)
     training_file_name = merge_results.union_algorithms_performance(args.mm)
-    model(args.DF, parameters['depth'],parameters['trees'], parameters['l_r'], parameters['col_s'], parameters['sub_s'], parameters['min_c'],parameters['seed'] ,args.es,training_file_name, N_clustering_algs )
+    model(embedding_dir, parameters['depth'],parameters['trees'], parameters['lr'], parameters['col_s'], parameters['sub_s'], parameters['min_c'],parameters['seed'] ,args.es,training_file_name, N_clustering_algs, args.mm )
